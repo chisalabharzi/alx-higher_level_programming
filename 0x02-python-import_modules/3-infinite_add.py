dac0a374958267394bd4e7152160034862faad23
chisalabharzi <chisalabharzi17@gmail.com>
2023-06-08 07:31:43 -0700

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" program that prints result of addition of all aeguments """
+
+if __name__ == "__main__":
+    import sys
+
+    total = 0
+
+    for x in range(len(sys.argv) - 1):
+        total += int(sys.argv[x + 1])
+    print("{}".format(total))
