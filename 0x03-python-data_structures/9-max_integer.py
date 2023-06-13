b3cf6522573a516cd5ad12e03ecc32d0414547f5
chisalabharzi <chisalabharzi17@gmail.com>
2023-06-13 07:21:06 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pyhton3
+
+""" function that finds the biggest integer of a list """
+
+
+def max_interger(my_list=[]):
+    if len(my_list) == 0:
+        return "None"
+    else:
+        max = my_list[0]
+        for x in range(len(my_list)):
+            if my_list[x] > max:
+                max = my_list[x]
+                return max
