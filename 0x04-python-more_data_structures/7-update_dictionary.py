2b442534391f6702ee39bb2bceb6d566a6626434
chisalabharzi <chisalabharzi17@gmail.com>
2023-06-14 04:44:12 -0700

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" reaplace or add key/value pairs in a dictionary """
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+
+    return (a_dictionary)
