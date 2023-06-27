02211c7f48e73adfa24549301fa59acfabee6a05
chisalabharzi <chisalabharzi17@gmail.com>
2023-06-27 11:47:43 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" class that defines a square """
+
+
+class Square:
+    """ creating a class square """
+
+    def __init__(self, size):
+        """ Initialize variable """
+        self .__size = size
