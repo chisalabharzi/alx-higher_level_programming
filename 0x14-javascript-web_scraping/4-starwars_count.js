6af0db8605ca34425e291fd4549f8338fd46c85c
chisalabharzi <chisalabharzi17@gmail.com>
2024-01-04 12:37:52 -0800

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,35 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
+const characterId = '18';
+let count = 0;
 
-const apiUrl = process.argv[2];
-
-// Check if an API URL is provided as an argument
-if (!apiUrl) {
-  console.error('Please provide an API URL as an argument.');
-  process.exit(1);
-}
-
-// Character ID for Wedge Antilles
-const characterId = 18;
-
-// Make a GET request to the Star Wars API films endpoint
-request(apiUrl, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-    return;
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    data.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
+          count += 1;
+        }
+      });
+    });
+    console.log(count);
   }
-
-  // Check if the response status code is 200 (OK)
-  if (response.statusCode !== 200) {
-    console.error(`Error: Status Code ${response.statusCode}`);
-    return;
-  }
-
-  const filmsData = JSON.parse(body);
-
-  // Filter films where Wedge Antilles is present
-  const filmsWithWedge = filmsData.results.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
-
-  console.log(`Number of movies with Wedge Antilles: ${filmsWithWedge.length}`);
 });
